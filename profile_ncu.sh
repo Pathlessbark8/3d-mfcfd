a1d42d568e443d68f4ffbc7ac9419d7049b1bcb4
Dhruv <justdhs@gmail.com>
2022-11-10 12:26:39 +0530

    script for profiling changes

--- profile_ncu.sh
+++ profile_ncu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/apps/spack/opt/spack/linux-rocky8-zen/gcc-8.5.0/nvhpc-22.5-a7app2dbudoj4hau2tox76w2wirmwsd2/Linux_x86_64/22.5/compilers/bin/ncu  -o multi_gpu_reports/4000000/4000000_ncu_report_2_all -f --target-processes all  --profile-from-start no --import-source 1 --devices 0 --set full mpirun -np 2 install/execname
+/apps/spack/opt/spack/linux-rocky8-zen/nvhpc-22.5/cuda-11.7.1-iwuavb6fxlcwvnlma2yxa7zby74vjhom/bin/ncu  -o multi_gpu_reports/16000000/16000000_ncu_report_1_all -f --target-processes all  --profile-from-start no --import-source 1 --devices 0 --set full mpirun -np 1 install/execname
--- profile_nsys.sh
+++ profile_nsys.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/apps/spack/opt/spack/linux-rocky8-zen/gcc-8.5.0/nvhpc-22.5-a7app2dbudoj4hau2tox76w2wirmwsd2/Linux_x86_64/22.5/compilers/bin/nsys profile --stats=true -o multi_gpu_reports/16000000/16000000_nsys_report_2_all_1000_iters mpirun -np 2 install/execname 
+/apps/spack/opt/spack/linux-rocky8-zen/nvhpc-22.5/cuda-11.7.1-iwuavb6fxlcwvnlma2yxa7zby74vjhom/bin/nsys profile --stats=true -o multi_gpu_reports/16000000/16000000_nsys_report_1_all mpirun -np 1 install/execname 
